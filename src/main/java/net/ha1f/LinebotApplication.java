440fac7041159532720f3a6431f43e092763e39c
ha1f <half.ldk.hiz@gmail.com>
2017-04-02 23:49:42 +0900

    いろいろ1

--- src/main/java/net/ha1f/LinebotApplication.java
+++ src/main/java/net/ha1f/LinebotApplication.java
@@ -2,9 +2,12 @@ package net.ha1f;
 
 import com.linecorp.bot.client.LineMessagingService;
 import com.linecorp.bot.model.ReplyMessage;
+import com.linecorp.bot.model.event.BeaconEvent;
 import com.linecorp.bot.model.event.Event;
+import com.linecorp.bot.model.event.FollowEvent;
 import com.linecorp.bot.model.event.MessageEvent;
-import com.linecorp.bot.model.event.message.TextMessageContent;
+import com.linecorp.bot.model.event.message.*;
+import com.linecorp.bot.model.message.Message;
 import com.linecorp.bot.model.message.TextMessage;
 import com.linecorp.bot.model.response.BotApiResponse;
 import com.linecorp.bot.spring.boot.annotation.EventMapping;
@@ -26,12 +29,87 @@ public class LinebotApplication {
 		SpringApplication.run(LinebotApplication.class, args);
 	}
 
+    private static void logEvent(Event event) {
+        System.out.println("event: " + event);
+    }
+
     @EventMapping
     public void handleTextMessageEvent(MessageEvent<TextMessageContent> event) throws Exception {
         System.out.println("event: " + event);
+        final String text = event.getMessage().getText();
+        final Message message;
+        if (text.endsWith("やで")) {
+            message = new TextMessage(text);
+        } else if (text.contains("みきてぃ")) {
+            message = new TextMessage("みきてぃやん！おはよー！");
+        } else {
+            message = new TextMessage(text + "やで");
+        }
+        final BotApiResponse apiResponse = lineMessagingService
+                .replyMessage(new ReplyMessage(event.getReplyToken(), Collections.singletonList(message)))
+                .execute().body();
+        System.out.println("Sent messages: " + apiResponse);
+    }
+
+    @EventMapping
+    public void handleStickerMessage(MessageEvent<StickerMessageContent> event) throws Exception {
+        logEvent(event);
         final BotApiResponse apiResponse = lineMessagingService
                 .replyMessage(new ReplyMessage(event.getReplyToken(),
-                        Collections.singletonList(new TextMessage(event.getSource().getUserId()))))
+                        Collections.singletonList(new TextMessage("スタンプ送信ありがとうございます！" + event.getMessage().getPackageId() + " : " + event.getMessage().getStickerId()))))
+                .execute().body();
+        System.out.println("Sent messages: " + apiResponse);
+    }
+
+    @EventMapping
+    public void handleImageMessage(MessageEvent<ImageMessageContent> event) throws Exception {
+        logEvent(event);
+        final BotApiResponse apiResponse = lineMessagingService
+                .replyMessage(new ReplyMessage(event.getReplyToken(),
+                        Collections.singletonList(new TextMessage("画像送信ありがとうございます！"))))
+                .execute().body();
+        System.out.println("Sent messages: " + apiResponse);
+
+        // lineMessagingService.replyMessage(new ReplyMessage(event.getReplyToken(), Collections.singletonList(new ImagemapMessage())))
+    }
+
+    @EventMapping
+    public void handleVideoMessage(MessageEvent<VideoMessageContent> event) throws Exception {
+        logEvent(event);
+        final BotApiResponse apiResponse = lineMessagingService
+                .replyMessage(new ReplyMessage(event.getReplyToken(),
+                        Collections.singletonList(new TextMessage("動画送信ありがとうございます！"))))
+                .execute().body();
+        System.out.println("Sent messages: " + apiResponse);
+    }
+
+    @EventMapping
+    public void handleAudioMessage(MessageEvent<AudioMessageContent> event) throws Exception {
+        logEvent(event);
+        final BotApiResponse apiResponse = lineMessagingService
+                .replyMessage(new ReplyMessage(event.getReplyToken(),
+                        Collections.singletonList(new TextMessage("音声送信ありがとうございます！"))))
+                .execute().body();
+        System.out.println("Sent messages: " + apiResponse);
+    }
+
+    @EventMapping
+    public void handleFollowEvent(FollowEvent event) throws Exception {
+        logEvent(event);
+        final BotApiResponse apiResponse = lineMessagingService
+                .replyMessage(new ReplyMessage(event.getReplyToken(),
+                        Collections.singletonList(new TextMessage("友だち追加ありがとう〜\nあなたはだあれ？"))))
+                .execute().body();
+        System.out.println("Sent messages: " + apiResponse);
+    }
+
+    @EventMapping
+    public  void handleBeaconEvent(BeaconEvent event) throws Exception {
+        logEvent(event);
+        Message m1 = new TextMessage("ご来店ありがとうございます！");
+        final BotApiResponse apiResponse = lineMessagingService
+                .replyMessage(new ReplyMessage(event.getReplyToken(),
+                        Collections.singletonList(m1)))
                 .execute().body();
         System.out.println("Sent messages: " + apiResponse);
     }
