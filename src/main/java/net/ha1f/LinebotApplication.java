467ad67f9fc4855866a3f519ae1275e434a2fa2e
ha1f <half.ldk.hiz@gmail.com>
2017-04-02 23:18:10 +0900

    add line bot

--- src/main/java/net/ha1f/LinebotApplication.java
+++ src/main/java/net/ha1f/LinebotApplication.java
@@ -1,12 +1,43 @@
 package net.ha1f;
 
+import com.linecorp.bot.client.LineMessagingService;
+import com.linecorp.bot.model.ReplyMessage;
+import com.linecorp.bot.model.event.Event;
+import com.linecorp.bot.model.event.MessageEvent;
+import com.linecorp.bot.model.event.message.TextMessageContent;
+import com.linecorp.bot.model.message.TextMessage;
+import com.linecorp.bot.model.response.BotApiResponse;
+import com.linecorp.bot.spring.boot.annotation.EventMapping;
+import com.linecorp.bot.spring.boot.annotation.LineMessageHandler;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Collections;
+
 @SpringBootApplication
+@LineMessageHandler
 public class LinebotApplication {
+
+    @Autowired
+    private LineMessagingService lineMessagingService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(LinebotApplication.class, args);
 	}
+
+    @EventMapping
+    public void handleTextMessageEvent(MessageEvent<TextMessageContent> event) throws Exception {
+        System.out.println("event: " + event);
+        final BotApiResponse apiResponse = lineMessagingService
+                .replyMessage(new ReplyMessage(event.getReplyToken(),
+                        Collections.singletonList(new TextMessage(event.getSource().getUserId()))))
+                .execute().body();
+        System.out.println("Sent messages: " + apiResponse);
+    }
+
+    @EventMapping
+    public void defaultMessageEvent(Event event) {
+        System.out.println("event: " + event);
+    }
 }
